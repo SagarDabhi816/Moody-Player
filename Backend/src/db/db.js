7122a7835cad48bff28a72e518c474c899754617
Sagardabhi816 <sagardabhi816@gmail.com>
2026-01-24 22:52:47 +0530

    daily update 24-1

--- Backend/src/db/db.js
+++ Backend/src/db/db.js
@@ -1,1 +1,12 @@
 const mongoose = require("mongoose")
+
+
+function connectToDB(){
+    mongoose.connect(process.env.MONGODB_URL).then(()=>{
+            console.log("Database Is Connected")
+    }).catch((error)=>{
+        console.error("Error While Connecting To Databse",error)
+    })
+}
+
+module.exports = connectToDB
--- Frontend/src/components/Header.jsx
+++ Frontend/src/components/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
     <header
       className="w-1/2 mx-auto  select-none bg-white text-[#1b1b1b] flex gap-[0.1rem] items-center justify-between py-2 px-4 border-b border-gray-200"
     >
-      <img src="/download.png" alt="Icon Here" className="w-[2rem]" />
+      <img src="/headerIcon.png" alt="Icon Here" className="w-[2rem]" />
       <h3 className="font-bold text-[0.8rem]">MoodWave</h3>
     </header>
   );
